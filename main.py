7b810304efd6970c8a1a9b57d261377eb37f4258
Paulina Flores <pflores17@students.avc.edu>
2025-04-11 07:49:58 -0700

    main

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 def main():
 
     N = int(input('Enter the number N: '))
-    result = []
+    result = [2** i for i in range (N+1)]
+    
+    print(result)
 
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+
 
     ########################################
     # Do not delete the return statement
